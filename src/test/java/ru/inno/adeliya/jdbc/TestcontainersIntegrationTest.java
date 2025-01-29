175d9a25955de86210386d2f3662a46d723a1152
nalemian <adelianagimova@gmail.com>
2025-01-29 19:05:52 +0300

    делаю много вставок в таблицы, sout-ы пока не выводят данные

--- src/test/java/ru/inno/adeliya/jdbc/TestcontainersIntegrationTest.java
+++ src/test/java/ru/inno/adeliya/jdbc/TestcontainersIntegrationTest.java
@@ -6,6 +6,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
+import ru.inno.adeliya.jdbc.entity.DepartmentEntity;
+import ru.inno.adeliya.jdbc.entity.EmployeeEntity;
+import ru.inno.adeliya.jdbc.entity.OrganizationEntity;
+import ru.inno.adeliya.jdbc.repository.DepartmentRepository;
+import ru.inno.adeliya.jdbc.repository.EmployeeRepository;
+import ru.inno.adeliya.jdbc.repository.OrganizationRepository;
+
+import java.sql.*;
 
 public class TestcontainersIntegrationTest {
 
@@ -30,6 +38,65 @@ public class TestcontainersIntegrationTest {
                 .load();
         flyway.migrate();
     }
+
+    @Test
+    void insertALotOfData() {
+        try (Connection connection = DriverManager.getConnection(
+                postgres.getJdbcUrl(),
+                postgres.getUsername(),
+                postgres.getPassword()
+        )) {
+            OrganizationRepository organizationRepository = new OrganizationRepository(() -> connection);
+            DepartmentRepository departmentRepository = new DepartmentRepository(() -> connection);
+            EmployeeRepository employeeRepository = new EmployeeRepository(() -> connection);
+
+            for (int i = 1; i <= 5; i++) {
+                OrganizationEntity org = new OrganizationEntity(i+10, "Организация " + i, 124 + i);
+                org = organizationRepository.save(org);
+                for (int j = 1; j <= 10; j++) {
+                    DepartmentEntity dept = new DepartmentEntity(j+10,org.getId(), "Отдел " + j);
+                    dept = departmentRepository.save(dept);
+                    for (int k = 1; k <= 100; k++) {
+                        EmployeeEntity emp = new EmployeeEntity(k+10, "Сотрудник " + k, 10000 + (k * 10), dept.getId());
+                        employeeRepository.save(emp);
+                    }
+                }
+            }
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM organization")) {
+                if (resultSet.next()) {
+                    System.out.println(resultSet.getInt(1));
+                }
+            }
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM department")) {
+                if (resultSet.next()) {
+                    System.out.println(resultSet.getInt(1));
+                }
+            }
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM employee")) {
+                if (resultSet.next()) {
+                    System.out.println(resultSet.getInt(1));
+                }
+            }
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery("SELECT * FROM employee ORDER BY id LIMIT 5 OFFSET 5")) {
+                while (resultSet.next()) {
+                    System.out.println(
+                            "ID: " + resultSet.getInt("id") +
+                                    ", name: " + resultSet.getString("name") +
+                                    ", salary: " + resultSet.getInt("salary") +
+                                    ", department: " + resultSet.getInt("department")
+                    );
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Test
     void name() {
